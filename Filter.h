774ec041077520095404efa02dd9ea216464120b
gojakamina <am1347go-s@student.lu.se>
2020-03-09 11:28:04 +0000

    added filters

--- Filter.h
+++ Filter.h
@@ -7,13 +7,16 @@ class Filter
 {
 
 private:
-	// create the filter structure for 3rd order
-	Iir::Butterworth::LowPass<3> f;
+	// create the filter structure for 4rd order
+	Iir::Butterworth::BandPass<4> bp;
+	Iir::Butterworth::HighPass<4> hp;
 
 public: 
-       Filter(float samplingrate, float cutoff_frequency) {f.setup (samplingrate, cutoff_frequency);}
-
-       float  filterData(float value) {return f.filter(value);}
+       Filter(float samplingrate, float centerFrequency, double widthFrequency) {bp.setup (samplingrate, centerFrequency, widthFrequency);}
+       Filter(float samplingrate, float cutoff_frequency) {hp.setup (samplingrate, cutoff_frequency);}
+	
+       float bpFilter(float value) {return bp.filter(value);}
+       float hpFilter(float value) {return hp.filter(value);}
 };
 
 #endif
