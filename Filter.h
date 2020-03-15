5488be37fe9798dcb23ca7259366686854285091
gojakamina <am1347go-s@student.lu.se>
2020-03-15 13:40:26 +0000

    improved filter data

--- Filter.h
+++ Filter.h
@@ -8,15 +8,15 @@ class Filter
 
 private:
 	// create the filter structure for 4rd order
-	Iir::Butterworth::BandPass<4> bp;
-	Iir::Butterworth::HighPass<4> hp;
+	Iir::Butterworth::BandPass<8> bp;
+	Iir::Butterworth::LowPass<8> lp;
 
 public: 
        Filter(float samplingrate, float centerFrequency, double widthFrequency) {bp.setup (samplingrate, centerFrequency, widthFrequency);}
-       Filter(float samplingrate, float cutoff_frequency) {hp.setup (samplingrate, cutoff_frequency);}
-	
+       Filter(float samplingrate, float cutoff_frequency) {lp.setup (samplingrate, cutoff_frequency);}
+
        float bpFilter(float value) {return bp.filter(value);}
-       float hpFilter(float value) {return hp.filter(value);}
+       float lpFilter(float value) {return lp.filter(value);}
 };
 
 #endif
