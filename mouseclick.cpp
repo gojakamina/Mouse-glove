7f0e2d2c16fe45cbce0339e4609dedea9e48bc64
Yuzhen404 <60343291+Yuzhen404@users.noreply.github.com>
2020-03-20 11:24:18 +0000

    Update mouseclick.cpp

--- mouseclick.cpp
+++ mouseclick.cpp
@@ -4,26 +4,26 @@ void mouseclick()
 {
 	xdo_t* x = xdo_new(NULL);
 	wiringPiSetup();
-	pinMode(L, INPUT);
-	pinMode(R, INPUT);
-	pinMode(S, INPUT);
-	pinMode(M, OUTPUT);
+	pinMode(17, INPUT);//Left button
+	pinMode(27, INPUT);//Right button
+	pinMode(22, INPUT);//Switch
+	pinMode(18, OUTPUT);//Motor
 	while (true) {
-		if (digitalRead(L) == 0)
+		if (digitalRead(17) == 0)
 		{
 			xdo_mouse_down(x, CURRENTWINDOW, 1);
-			digitalWrite(M, HIGH); delay(100);
-			digitalWrite(M, LOW); delay(100);
+			digitalWrite(18, HIGH); delay(100);
+			digitalWrite(18, LOW); delay(100);
 		}
 		else
 		{
 			xdo_mouse_up(x, CURRENTWINDOW, 1);
 		}
-		if (digitalRead(R) == 0)
+		if (digitalRead(27) == 0)
 		{
 			xdo_mouse_down(x, CURRENTWINDOW, 3);
-			digitalWrite(M, HIGH); delay(100);
-			digitalWrite(M, LOW); delay(100);
+			digitalWrite(18, HIGH); delay(100);
+			digitalWrite(18, LOW); delay(100);
 		}
 		else
 		{
