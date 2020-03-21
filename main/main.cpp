11a4fcf7175a4b05c9f34834a6bd0da142e55065
TongHeUoG <xiyingluht@gmail.com>
2020-03-21 01:02:45 +0000

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   CMakeLists.txt
    	renamed:    LSM9DS1_demo.cpp -> main.cpp
    
     Changes not staged for commit:
    	modified:   ../CMakeLists.txt

--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,7 @@
+extern "C" {
+#include <xdo.h>
+}
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -5,14 +9,28 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <cmath>
+
 #include "LSM9DS1_Types.h"
 #include "LSM9DS1.h"
 #include "Filter.h"
+
+#include <pthread.h> 
+#include <iostream>
+#include <wiringPi.h>
+
+#define Left_Key_down  0 //PIN11
+#define Left_Key_up    1 //PIN12
+#define Right_Key_down 2 //PIN13
+#define Right_key_up   3 //Pin15
+#define reset		   4 //Pin16
+#define Motor		   5 //Pin18
+
 
 float currAccZ, currAccY, currVelZ, currVelY, currPosZ, currPosY, prevAccZ, prevAccY, prevVelZ, prevVelY, prevPosZ, prevPosY = 0;
 float dT = 0.01;
 float conv = 9.80665;
 float thresh  = 0.06;
+float dpi = 1920/53.15 //24' 1080p
 int countZ, countY = 0;
 
 
@@ -23,6 +41,66 @@ const float cutoff_frequency = 5;
 // filter order is determined in the class found in Filter.h, should be changed
 Filter lowpass(samplingrate, cutoff_frequency);
 
+xdo_t * x = xdo_new(NULL);
+
+
+void mouse_upL(void){
+	xdo_mouse_up(x, CURRENTWINDOW, 1);
+	//std::cout << "release\n";
+}
+
+void mouse_upR(void){
+	xdo_mouse_up(x, CURRENTWINDOW, 2);
+	//std::cout << "release\n";
+}
+
+void mouse_downL(void){
+	//anti-shake
+	delay(20);
+	if(digitalRead(Left_Key_down)){
+		xdo_mouse_down(x, CURRENTWINDOW, 3);
+		//std::cout << "press\n";
+	}
+}
+
+void mouse_downR(void){
+	//anti-shake
+	delay(20);
+	if(digitalRead(Left_Key_down)){
+		xdo_mouse_down(x, CURRENTWINDOW, 3);
+		//std::cout << "press\n";
+	}
+}
+
+//thread for Click event
+void *MouseClik(void *args){
+	pinMode(Left_Key_down,INPUT);
+	pinMode(Left_Key_up,INPUT);
+	
+	pinMode(Right_Key_down,INPUT);
+	pinMode(Right_Key_up,INPUT);
+		
+	//setup interrupte
+	wiringPiISR (Left_Key_up, INT_EDGE_FALLING, &mouse_upL);
+	wiringPiISR (Left_Key_down, INT_EDGE_RISING, &mouse_downL);
+	wiringPiISR (Right_Key_up, INT_EDGE_FALLING, &mouse_upR);
+	wiringPiISR (Right_Key_down, INT_EDGE_RISING, &mouse_downR);
+}
+
+
+void positionReset() {
+	currPosZ = 0;
+	currPosY = 0;
+	digitalWrite(Motor, HIGH);
+}
+	
+//thread for reset position
+void *reset(void *args){
+	pinMode(reset,INPUT);
+	pinMode(Motor, OUTPUT);
+	wiringPiISR (reset, INT_EDGE_FALLING, &positionReset);
+}
+	
 
 /**
  * Threshold used for accelerometer data. Prevents values lower than a specified limit.
@@ -123,7 +201,7 @@ class LSM9DS1printCallback : public LSM9DS1callback {
 
 
 		// print values
-		printf("Acc, vel & pos: %f, %f, %f, %f, %f, %f \n", currAccZ, currVelZ, currPosZ*conv, currAccY, currVelY, currPosY*conv);
+		//printf("Acc, vel & pos: %f, %f, %f, %f, %f, %f \n", currAccZ, currVelZ, currPosZ*conv, currAccY, currVelY, currPosY*conv);
 
 
 		// set current values to previous
@@ -135,18 +213,27 @@ class LSM9DS1printCallback : public LSM9DS1callback {
 
 		prevPosZ = currPosZ;
 		prevPosY = currPosY;
-
+		
+		xdo_mouse_move(x, currPosY*conv*dpi*100, currPosY*conv*dpi*100,0);
 	}
 };
 
 int main(int argc, char *argv[]) {
+	
+	wiringPiSetup();
+	pthread_t click,reset;
+	pthread_create(&click,NULL,MouseClik,NULL);
+	pthread_create(&reset,NULL,reset,NULL);
+	
     LSM9DS1 imu(IMU_MODE_I2C, 0x6b, 0x1e);
     LSM9DS1printCallback callback;
     imu.setCallback(&callback);
     imu.begin();
+    
     do {
 	sleep(1);
     } while (getchar() != 27);
     imu.end();
+    
     exit(EXIT_SUCCESS);
 }
